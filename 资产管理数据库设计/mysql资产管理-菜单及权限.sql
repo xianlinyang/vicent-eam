a2bc697fec17b3cfe254201b1699851c564fa9eb
13725578420 <349134174@qq.com>
2022-08-10 21:33:38 +0800

    增加脚本

---
+++ 资产管理数据库设计/mysql资产管理-菜单及权限.sql
@@ -0,0 +1,189 @@
+﻿insert into tbl_menu(f_qxid,f_pid,f_code,f_name,f_alias,f_component,f_status,f_type,f_level,f_orderid,f_create_user,f_create_time,f_flag)
+values(1,0,'','基础资料','基础资料',0,1,1,1,1,1,NOW(),0);
+
+      insert into tbl_menu(f_qxid,f_pid,f_code,f_name,f_alias,f_component,f_status,f_type,f_level,f_orderid,f_create_user,f_create_time,f_flag)
+      values(101,1,'Deptno','部门信息','部门信息',0,1,1,2,1,1,NOW(),0);
+      insert into tbl_menu(f_qxid,f_pid,f_code,f_name,f_alias,f_component,f_status,f_type,f_level,f_orderid,f_create_user,f_create_time,f_flag)
+      values(102,1,'AssetsType','资产类型','资产类型',0,1,1,2,2,1,NOW(),0); 
+      insert into tbl_menu(f_qxid,f_pid,f_code,f_name,f_alias,f_component,f_status,f_type,f_level,f_orderid,f_create_user,f_create_time,f_flag)
+      values(103,1,'BuyModel','购置方式','购置方式',0,1,1,2,3,1,NOW(),0); 
+      insert into tbl_menu(f_qxid,f_pid,f_code,f_name,f_alias,f_component,f_status,f_type,f_level,f_orderid,f_create_user,f_create_time,f_flag)
+      values(104,1,'StoreAddress','存放地点','存放地点',0,1,1,2,4,1,NOW(),0); 
+      insert into tbl_menu(f_qxid,f_pid,f_code,f_name,f_alias,f_component,f_status,f_type,f_level,f_orderid,f_create_user,f_create_time,f_flag)
+      values(105,1,'SavePeople','保管人/使用人','保管人/使用人',0,1,1,2,5,1,NOW(),0); 
+      insert into tbl_menu(f_qxid,f_pid,f_code,f_name,f_alias,f_component,f_status,f_type,f_level,f_orderid,f_create_user,f_create_time,f_flag)
+      values(106,1,'AssetsUnit','资产单位','资产单位',0,1,1,2,6,1,NOW(),0); 
+      insert into tbl_menu(f_qxid,f_pid,f_code,f_name,f_alias,f_component,f_status,f_type,f_level,f_orderid,f_create_user,f_create_time,f_flag)
+      values(107,1,'AssetsUse','资产用途','资产用途',0,1,1,2,7,1,NOW(),0); 
+      insert into tbl_menu(f_qxid,f_pid,f_code,f_name,f_alias,f_component,f_status,f_type,f_level,f_orderid,f_create_user,f_create_time,f_flag)
+      values(108,1,'AssetsState','资产当前状态','资产当前状态',0,1,1,2,8,1,NOW(),0); 
+      insert into tbl_menu(f_qxid,f_pid,f_code,f_name,f_alias,f_component,f_status,f_type,f_level,f_orderid,f_create_user,f_create_time,f_flag)
+      values(109,1,'Supplier','供应商','供应商',0,1,1,2,9,1,NOW(),0); 
+
+      insert into tbl_menu(f_qxid,f_pid,f_code,f_name,f_alias,f_component,f_status,f_type,f_level,f_orderid,f_create_user,f_create_time,f_flag)
+      values(111,1,'Dictionary','数据字典','数据字典',0,1,1,2,11,1,NOW(),0); 
+
+
+insert into tbl_menu(f_qxid,f_pid,f_code,f_name,f_alias,f_component,f_status,f_type,f_level,f_orderid,f_create_user,f_create_time,f_flag)
+values(2,0,'','资产清单','资产清单',0,1,1,1,2,1,NOW(),0);
+
+      insert into tbl_menu(f_qxid,f_pid,f_code,f_name,f_alias,f_component,f_status,f_type,f_level,f_orderid,f_create_user,f_create_time,f_flag)
+      values(201,2,'AssetsList','资产列表','资产列表',0,1,1,2,1,1,NOW(),0); 
+
+ 
+insert into tbl_menu(f_qxid,f_pid,f_code,f_name,f_alias,f_component,f_status,f_type,f_level,f_orderid,f_create_user,f_create_time,f_flag)
+values(3,0,'','资产入库','资产入库',0,1,1,1,3,1,NOW(),0);
+      insert into tbl_menu(f_qxid,f_pid,f_code,f_name,f_alias,f_component,f_status,f_type,f_level,f_orderid,f_create_user,f_create_time,f_flag)
+      values(301,3,'AssetInstock','采购入库','采购入库',0,1,1,2,1,1,NOW(),0); 
+      insert into tbl_menu(f_qxid,f_pid,f_code,f_name,f_alias,f_component,f_status,f_type,f_level,f_orderid,f_create_user,f_create_time,f_flag)
+      values(302,3,'AssetsPurchase','采购申请','采购入库',0,1,1,2,2,1,NOW(),0); 
+
+
+insert into tbl_menu(f_qxid,f_pid,f_code,f_name,f_alias,f_component,f_status,f_type,f_level,f_orderid,f_create_user,f_create_time,f_flag)
+values(4,0,'','资产操作','资产操作',0,1,1,1,4,1,NOW(),0);
+      insert into tbl_menu(f_qxid,f_pid,f_code,f_name,f_alias,f_component,f_status,f_type,f_level,f_orderid,f_create_user,f_create_time,f_flag)
+      values(401,4,'AssetReceive','资产领用','资产领用',0,1,1,2,1,1,NOW(),0); 
+      insert into tbl_menu(f_qxid,f_pid,f_code,f_name,f_alias,f_component,f_status,f_type,f_level,f_orderid,f_create_user,f_create_time,f_flag)
+      values(402,4,'AssetsLend','资产借用','资产借用',0,1,1,2,2,1,NOW(),0);
+      insert into tbl_menu(f_qxid,f_pid,f_code,f_name,f_alias,f_component,f_status,f_type,f_level,f_orderid,f_create_user,f_create_time,f_flag)
+      values(403,4,'AssetsChange','资产变更','资产变更',0,1,1,2,3,1,NOW(),0);
+      insert into tbl_menu(f_qxid,f_pid,f_code,f_name,f_alias,f_component,f_status,f_type,f_level,f_orderid,f_create_user,f_create_time,f_flag)
+      values(404,4,'AssetsAllot','资产调拨','资产调拨',0,1,1,2,4,1,NOW(),0);
+      insert into tbl_menu(f_qxid,f_pid,f_code,f_name,f_alias,f_component,f_status,f_type,f_level,f_orderid,f_create_user,f_create_time,f_flag)
+      values(405,4,'AssetsBack','资产退还','资产退还',0,1,1,2,5,1,NOW(),0);
+
+
+insert into tbl_menu(f_qxid,f_pid,f_code,f_name,f_alias,f_component,f_status,f_type,f_level,f_orderid,f_create_user,f_create_time,f_flag)
+values(5,0,'','资产处置','资产处置',0,1,1,1,5,1,NOW(),0);
+      insert into tbl_menu(f_qxid,f_pid,f_code,f_name,f_alias,f_component,f_status,f_type,f_level,f_orderid,f_create_user,f_create_time,f_flag)
+      values(501,5,'AssetRepair','资产维修','资产维修',0,1,1,2,1,1,NOW(),0); 
+      insert into tbl_menu(f_qxid,f_pid,f_code,f_name,f_alias,f_component,f_status,f_type,f_level,f_orderid,f_create_user,f_create_time,f_flag)
+      values(502,5,'AssetsScrap','资产报废','资产报废',0,1,1,2,2,1,NOW(),0);
+      insert into tbl_menu(f_qxid,f_pid,f_code,f_name,f_alias,f_component,f_status,f_type,f_level,f_orderid,f_create_user,f_create_time,f_flag)
+      values(503,5,'AssetsContinue','资产续用','资产续用',0,1,1,2,3,1,NOW(),0);
+      insert into tbl_menu(f_qxid,f_pid,f_code,f_name,f_alias,f_component,f_status,f_type,f_level,f_orderid,f_create_user,f_create_time,f_flag)
+      values(504,5,'AssetsEliminate','资产销账','资产销账',0,1,1,2,4,1,NOW(),0);
+ 
+
+insert into tbl_menu(f_qxid,f_pid,f_code,f_name,f_alias,f_component,f_status,f_type,f_level,f_orderid,f_create_user,f_create_time,f_flag)
+values(6,0,'','业务申请','业务申请',0,1,1,1,6,1,NOW(),0);
+      insert into tbl_menu(f_qxid,f_pid,f_code,f_name,f_alias,f_component,f_status,f_type,f_level,f_orderid,f_create_user,f_create_time,f_flag)
+      values(601,6,'InstockApply','采购申请','采购申请',0,1,1,2,1,1,NOW(),0); 
+      insert into tbl_menu(f_qxid,f_pid,f_code,f_name,f_alias,f_component,f_status,f_type,f_level,f_orderid,f_create_user,f_create_time,f_flag)
+      values(602,6,'ReceiveApply','领用申请','领用申请',0,1,1,2,2,1,NOW(),0);
+      insert into tbl_menu(f_qxid,f_pid,f_code,f_name,f_alias,f_component,f_status,f_type,f_level,f_orderid,f_create_user,f_create_time,f_flag)
+      values(603,6,'ScrapApply','报废申请','报废申请',0,1,1,2,3,1,NOW(),0);
+      insert into tbl_menu(f_qxid,f_pid,f_code,f_name,f_alias,f_component,f_status,f_type,f_level,f_orderid,f_create_user,f_create_time,f_flag)
+      values(604,6,'LendApply','借用申请','借用申请',0,1,1,2,4,1,NOW(),0);
+      insert into tbl_menu(f_qxid,f_pid,f_code,f_name,f_alias,f_component,f_status,f_type,f_level,f_orderid,f_create_user,f_create_time,f_flag)
+      values(605,6,'RepairApply','维修申请','维修申请',0,1,1,2,5,1,NOW(),0);
+ 
+
+insert into tbl_menu(f_qxid,f_pid,f_code,f_name,f_alias,f_component,f_status,f_type,f_level,f_orderid,f_create_user,f_create_time,f_flag)
+values(7,0,'','盘点管理','盘点管理',0,1,1,1,7,1,NOW(),0);
+      insert into tbl_menu(f_qxid,f_pid,f_code,f_name,f_alias,f_component,f_status,f_type,f_level,f_orderid,f_create_user,f_create_time,f_flag)
+      values(701,7,'DamageList','资产损益列表','资产损益列表',0,1,1,2,1,1,NOW(),0); 
+      insert into tbl_menu(f_qxid,f_pid,f_code,f_name,f_alias,f_component,f_status,f_type,f_level,f_orderid,f_create_user,f_create_time,f_flag)
+      values(702,7,'AssetsInventory','资产盘点','资产盘点',0,1,1,2,2,1,NOW(),0);
+      insert into tbl_menu(f_qxid,f_pid,f_code,f_name,f_alias,f_component,f_status,f_type,f_level,f_orderid,f_create_user,f_create_time,f_flag)
+      values(703,7,'ConsumInventory','耗材盘点','耗材盘点',0,1,1,2,3,1,NOW(),0);
+      insert into tbl_menu(f_qxid,f_pid,f_code,f_name,f_alias,f_component,f_status,f_type,f_level,f_orderid,f_create_user,f_create_time,f_flag)
+      values(704,7,'ConsumDamageList','耗材损益列表','耗材损益列表',0,1,1,2,4,1,NOW(),0);
+   
+
+insert into tbl_menu(f_qxid,f_pid,f_code,f_name,f_alias,f_component,f_status,f_type,f_level,f_orderid,f_create_user,f_create_time,f_flag)
+values(8,0,'','分析报表','分析报表',0,1,1,1,8,1,NOW(),0);
+      insert into tbl_menu(f_qxid,f_pid,f_code,f_name,f_alias,f_component,f_status,f_type,f_level,f_orderid,f_create_user,f_create_time,f_flag)
+      values(801,8,'Report_As_Type','资产分类','资产分类',0,1,1,2,1,1,NOW(),0); 
+ 
+
+
+insert into tbl_menu(f_qxid,f_pid,f_code,f_name,f_alias,f_component,f_status,f_type,f_level,f_orderid,f_create_user,f_create_time,f_flag)
+values(9,0,'','设置','设置',0,1,1,1,9,1,NOW(),0);
+      insert into tbl_menu(f_qxid,f_pid,f_code,f_name,f_alias,f_component,f_status,f_type,f_level,f_orderid,f_create_user,f_create_time,f_flag)
+      values(901,9,'Dept','组织机构','组织机构',0,1,1,2,1,1,NOW(),0); 
+      insert into tbl_menu(f_qxid,f_pid,f_code,f_name,f_alias,f_component,f_status,f_type,f_level,f_orderid,f_create_user,f_create_time,f_flag)
+      values(902,9,'Role','角色管理','角色管理',0,1,1,2,2,1,NOW(),0);
+      insert into tbl_menu(f_qxid,f_pid,f_code,f_name,f_alias,f_component,f_status,f_type,f_level,f_orderid,f_create_user,f_create_time,f_flag)
+      values(903,9,'User','用户管理','用户管理',0,1,1,2,3,1,NOW(),0);
+      insert into tbl_menu(f_qxid,f_pid,f_code,f_name,f_alias,f_component,f_status,f_type,f_level,f_orderid,f_create_user,f_create_time,f_flag)
+      values(904,9,'SysLog','操作日志','操作日志',0,1,1,2,4,1,NOW(),0);
+
+ 
+
+INSERT INTO tbl_perms(f_qxid,f_menu_id,f_name,f_code,f_api,f_method,f_status,f_orderid,f_create_user,f_create_time,f_flag)
+SELECT * FROM (
+
+SELECT CONCAT(f_qxid,'01') AS qxid,f_qxid,'浏览' AS f_name,CONCAT(f_code,':browse')AS d,CONCAT(f_code,'/browse') AS ff,1 AS f_method,1 f_status,1 f_orderid,1 f_create_user,now() f_create_time,0 f_flat FROM tbl_menu WHERE f_qxid = 111
+UNION all
+SELECT CONCAT(f_qxid,'02') AS qxid,f_qxid,'新增' AS f_name,CONCAT(f_code,':create')AS d,CONCAT(f_code,'/create') AS ff,2 AS f_method ,1 f_status,2 f_orderid,1 f_create_user,now() f_create_time,0 f_flat FROM tbl_menu WHERE f_qxid = 111
+UNION all
+SELECT CONCAT(f_qxid,'03') AS qxid,f_qxid,'修改' AS f_name,CONCAT(f_code,':update')AS d,CONCAT(f_code,'/update') AS ff,3 AS f_method ,1 f_status,3 f_orderid,1 f_create_user,now() f_create_time,0 f_flat FROM tbl_menu WHERE f_qxid = 111
+UNION all
+SELECT CONCAT(f_qxid,'04') AS qxid,f_qxid,'删除' AS f_name,CONCAT(f_code,':delete')AS d,CONCAT(f_code,'/delete') AS ff,4 AS f_method ,1 f_status,4 f_orderid,1 f_create_user,now() f_create_time,0 f_flat FROM tbl_menu WHERE f_qxid = 111
+
+) a ORDER BY f_qxid,f_orderid
+
+
+--加权限
+  INSERT INTO tbl_role_data_perms(f_roleid,f_qxid,f_qx_pid,f_accountid)
+  SELECT 1,f_qxid,f_menu_id,1 FROM tbl_perms WHERE f_qxid NOT IN (SELECT f_qxid FROM tbl_role_data_perms WHERE f_roleid = 1)
+
+  INSERT INTO tbl_perms(f_qxid,f_menu_id,f_name,f_code,f_api,f_method,f_status,f_orderid,f_create_user,f_create_time,f_flag)
+  VALUES(90305,903,'启用/停用','User:SetStatus','User/SetStatus',5,1,5,1,now(),0)
+
+  INSERT INTO tbl_perms(f_qxid,f_menu_id,f_name,f_code,f_api,f_method,f_status,f_orderid,f_create_user,f_create_time,f_flag)
+  VALUES(90306,903,'修改密码','User:SetPwd','User/SetPwd',5,1,6,1,now(),0)
+ 
+--部门加启用停用
+  INSERT INTO tbl_perms(f_qxid,f_menu_id,f_name,f_code,f_api,f_method,f_status,f_orderid,f_create_user,f_create_time,f_flag)
+  VALUES(10105,101,'启用/停用','Deptno:SetStatus','Deptno/SetStatus',5,1,5,1,now(),0)
+
+
+
+INSERT INTO tbl_role_data_perms(f_roleid,f_qxid,f_qx_pid,f_accountid)
+VALUES(1,10105,101,0)
+
+
+INSERT INTO tbl_perms(f_qxid,f_menu_id,f_name,f_code,f_api,f_method,f_status,f_orderid,f_create_user,f_create_time,f_flag)
+SELECT * FROM (
+
+SELECT CONCAT( CAST(f_qxid AS char),'01')  AS id,f_qxid,'浏览' AS f_name,CONCAT(f_code,':browse')AS d,CONCAT(f_code,'/browse') AS ff,1 AS f_method,1 f_status,1 f_orderid,1 f_create_user,now() f_create_time,0 f_flat FROM tbl_menu WHERE f_qxid = 905
+UNION all
+SELECT CONCAT( CAST(f_qxid AS char),'02') AS id,f_qxid,'新增' AS f_name,CONCAT(f_code,':create')AS d,CONCAT(f_code,'/create') AS ff,2 AS f_method ,1 f_status,2 f_orderid,1 f_create_user,now() f_create_time,0 f_flat FROM tbl_menu WHERE f_qxid = 905
+UNION all
+SELECT CONCAT( CAST(f_qxid AS char),'03') AS id, f_qxid,'修改' AS f_name,CONCAT(f_code,':update')AS d,CONCAT(f_code,'/update') AS ff,3 AS f_method ,1 f_status,3 f_orderid,1 f_create_user,now() f_create_time,0 f_flat FROM tbl_menu WHERE f_qxid = 905
+UNION ALL
+SELECT CONCAT( CAST(f_qxid AS char),'04') AS ID , f_qxid,'删除' AS f_name,CONCAT(f_code,':delete')AS d,CONCAT(f_code,'/delete') AS ff,4 AS f_method ,1 f_status,4 f_orderid,1 f_create_user,now() f_create_time,0 f_flat FROM tbl_menu WHERE f_qxid = 905
+UNION all
+SELECT CONCAT( CAST(f_qxid AS char),'05') AS ID , f_qxid,'启用' AS f_name,CONCAT(f_code,':delete')AS d,CONCAT(f_code,'/delete') AS ff,5 AS f_method ,1 f_status,4 f_orderid,1 f_create_user,now() f_create_time,0 f_flat FROM tbl_menu WHERE f_qxid = 905
+UNION all
+SELECT CONCAT( CAST(f_qxid AS char),'06') AS ID , f_qxid,'停用' AS f_name,CONCAT(f_code,':delete')AS d,CONCAT(f_code,'/delete') AS ff,6 AS f_method ,1 f_status,4 f_orderid,1 f_create_user,now() f_create_time,0 f_flat FROM tbl_menu WHERE f_qxid = 905
+
+) a ORDER BY f_qxid,f_orderid
+
+INSERT INTO tbl_perms(f_qxid,f_menu_id,f_name,f_code,f_api,f_method,f_status,f_orderid,f_create_user,f_create_time,f_flag)
+SELECT * FROM (
+
+SELECT CONCAT(f_qxid,'05') AS qxid,f_qxid,'拒绝' AS f_name,CONCAT(f_code,':refuse')AS d,CONCAT(f_code,'/refuse') AS ff,5 AS f_method,1 f_status,5 f_orderid,1 f_create_user,now() f_create_time,0 f_flat FROM tbl_menu WHERE f_qxid = 501
+UNION all
+SELECT CONCAT(f_qxid,'06') AS qxid,f_qxid,'送修' AS f_name,CONCAT(f_code,':confirm')AS d,CONCAT(f_code,'/confirm') AS ff,6 AS f_method ,1 f_status,6 f_orderid,1 f_create_user,now() f_create_time,0 f_flat FROM tbl_menu WHERE f_qxid = 501
+UNION all
+SELECT CONCAT(f_qxid,'07') AS qxid,f_qxid,'修理完成' AS f_name,CONCAT(f_code,':repair')AS d,CONCAT(f_code,'/repair') AS ff,7 AS f_method ,1 f_status,7 f_orderid,1 f_create_user,now() f_create_time,0 f_flat FROM tbl_menu WHERE f_qxid = 501
+UNION all
+SELECT CONCAT(f_qxid,'08') AS qxid,f_qxid,'交付完成' AS f_name,CONCAT(f_code,':complete')AS d,CONCAT(f_code,'/complete') AS ff,8 AS f_method ,1 f_status,8 f_orderid,1 f_create_user,now() f_create_time,0 f_flat FROM tbl_menu WHERE f_qxid = 501
+
+) a ORDER BY f_qxid,f_orderid;
+
+
+--插入角色权限
+INSERT INTO tbl_role_data_perms(f_roleid,f_qxid,f_qx_pid,f_accountid)
+SELECT 1,f_qxid,f_menu_id,1 FROM tbl_perms  WHERE f_qxid NOT IN 
+(SELECT f_qxid FROM tbl_role_data_perms WHERE f_roleid = 1) 
+
+
+
+  INSERT INTO tbl_perms(f_qxid,f_menu_id,f_name,f_code,f_api,f_method,f_status,f_orderid,f_create_user,f_create_time,f_flag,f_accountid)
+  VALUES(60205,602,'主管审批','ReceiveApply:ManagerApproval','ReceiveApply/ManagerApproval',5,1,5,1,NOW(),0,0);
+  INSERT INTO tbl_perms(f_qxid,f_menu_id,f_name,f_code,f_api,f_method,f_status,f_orderid,f_create_user,f_create_time,f_flag,f_accountid)
+  VALUES(60205,602,'仓库审批','ReceiveApply:AsseetApproval','ReceiveApply/AsseetApproval',6,1,6,1,NOW(),0,0);
